b8c27ec830418a871b7c5bc3fe8fc111e8082bcc
Roy Segall <roy.segall@gizra.com>
2017-03-11 21:13:42 +0200

    Fix the title validator.

--- include/DevDaysRestfulExampleValidatorTrait.php
+++ include/DevDaysRestfulExampleValidatorTrait.php
@@ -20,7 +20,7 @@ trait DevDaysRestfulExampleValidatorTrait {
    *   The wrapped property.
    */
   public function TitleLengthValidation($field_name, $value, EntityMetadataWrapper $wrapper, EntityMetadataWrapper $property_wrapper) {
-    if (strlen($value) < 3) {
+    if (strlen($value['value']) < 3) {
       $this->setError($field_name, 'The @field should be at least 3 characters long.');
     }
   }
